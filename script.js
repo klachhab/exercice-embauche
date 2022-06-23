68c3a2bf58ab82a78dd9122f0a50545d7da05d4d
klachhab <klachhab@icloud.com>
2022-06-23 19:35:46 +0100

    Lister la liste des Users dans le tableau
    + Ajout d'une icone de suppression
    + Form as a Modal

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let users = [{
+var users = [
+    {
         id: "123456789",
-        createdDate: "2021-01-06T00:00:00.000Z",
+        createdDate: "2021-01-06",
         status: "En validation",
         firstName: "Mohamed",
         lastName: "Taha",
@@ -9,7 +10,7 @@ let users = [{
     },
     {
         id: "987654321",
-        createdDate: "2021-07-25T00:00:00.000Z",
+        createdDate: "2021-07-25",
         status: "Validé",
         firstName: "Hamid",
         lastName: "Orrich",
@@ -18,7 +19,7 @@ let users = [{
     },
     {
         id: "852963741",
-        createdDate: "2021-09-15T00:00:00.000Z",
+        createdDate: "2021-09-15",
         status: "Rejeté",
         firstName: "Rachid",
         lastName: "Mahidi",
@@ -27,19 +28,116 @@ let users = [{
     }
 ]
 
-const addBtn = document.querySelector("#addBtn")
-const listTable = document.querySelector("tbody#user-list")
-var infosRows = document.querySelectorAll(".infos-rows")
+var usersTable = document.querySelector("table#users-table")
 
-const formData = document.querySelector("#formData")
+function insertCell(userObject, index) {
+    var userRow = usersTable.insertRow()
+    const emptyCell = document.getElementById("emptyCell")
 
-addBtn.addEventListener("click", (event) => {
-    // console.dir(event.target)
-    console.dir(infosRows)
+    if (emptyCell) {
+        emptyCell.remove()
+    }
+
+    for (const property in userObject) {
+        const cellClasses = ["border-b", "border-slate-100", "p-4", "pl-8", "text-slate-500"]
+
+        var cell = userRow.insertCell()
+        cell.classList.value = cellClasses.join(" ")
+        if (property === "status") {
+            const spanClasses = ['w-full', 'p-1.5', 'rounded', "text-white"]
+
+            let span = document.createElement("span")
+            span.classList.value = spanClasses.join(" ")
+            span.innerText = userObject[property]
+
+            switch (userObject[property]) {
+                case "En validation":
+                    span.classList.add("on-validation")
+                    break;
+
+                case "Validé":
+                    span.classList.add("valide")
+                    break;
+
+                case "Rejeté":
+                    span.classList.add("rejected")
+                    break;
+            }
+
+            cell.appendChild(span)
+        } 
+        else {
+            cell.innerText = userObject[property]
+        }
+    }
+
+    // Ajout d'icone de suppression :
+
+    var deleteCell = userRow.insertCell()
+    deleteCell.classList.value = ['flex justify-center p-4', "border-b", "border-slate-100"].join(" ")
+    
+    var img = document.createElement("img")
+    img.setAttribute("src", "./trash.png")
+    img.classList.value = ['h-5', 'cursor-pointer'].join(" ")
+
+    deleteCell.appendChild(img)
+
+    img.addEventListener("click", () => {
+        users.splice(index, 1)
+        userRow.remove()
+
+        // localStorage.setItem('users', JSON.stringify(users))
+
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Get users from local storage
+    // users = JSON.parse(localStorage.getItem('users'))
+    users.forEach((user, index) => {
+        insertCell(user, index)
+    });
 })
 
-formData.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const data = new FormData(formData)
-    console.log(data);
-})
+
+const form = document.querySelector("form#form")
+
+// Adding new User Form
+function submitForm() {
+
+    const userData = Array.from(form.children)
+        .map(el => el.lastElementChild)
+        .reduce(function (result, item) {
+            result[item.name] = item.value;
+            return result;
+        }, {})
+
+
+    userData.createdDate = moment(userData['createdDate']).format("DD/MM/YYYY")
+    const id = users.length ? users[users.length - 1].id + 1 : 1
+    const data = Object.assign({
+        id
+    }, userData)
+
+    insertCell(data)
+
+    users.push(data)
+    
+    // Add to local storage
+    // localStorage.setItem('users', JSON.stringify(users))
+
+    toggleModal(false)
+}
+
+
+// Toggle Modal
+
+function toggleModal(testing) {
+    let modal = document.querySelector("#modal_overlay")
+    if (testing) {
+        modal.classList.remove("hidden")
+    }
+    else modal.classList.add("hidden")
+    
+}
